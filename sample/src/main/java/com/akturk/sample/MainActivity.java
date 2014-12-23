d8373e8dddcf7bc6bde2c8fe46e2457bb5acc236
Emre Akturk <akturke@netas.com.tr>
2014-12-23 16:50:43 +0200

    Sample is added.

---
+++ sample/src/main/java/com/akturk/sample/MainActivity.java
@@ -0,0 +1,16 @@
+package com.akturk.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+//    private ContextualView mContextualView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_main);
+    }
+
+}
